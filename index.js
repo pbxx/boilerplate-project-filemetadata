ff1465abab45a85f5c13d839da879027ebfc0a59
Donavan Lewis <airlew@gmail.com>
2024-07-28 16:40:52 -0600

    working up to fCC spec

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express")
+var cors = require("cors")
+const multer = require("multer")
+require("dotenv").config()
 
-var app = express();
+const upload = multer({ dest: "uploads/" })
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html")
+})
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+	// console.log(req.file)
+	res.send({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+})
 
-
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+	console.log("Your app is listening on port " + port)
+})
